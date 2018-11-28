9698ff49263531ee9345c81074b63233f46b1da9
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-11-28 16:30:56 +0100

    Save tokens

--- front/main.js
+++ front/main.js
@@ -1,3 +1,7 @@
+var socketURL = 'http://localhost:3001'
+var securifyURL = 'http://localhost:3001/securify'
+var profileURL = 'http://localhost:3000/'
+
 var app = new Vue({
   el: '#app',
   data: () => ({
@@ -17,7 +21,7 @@ var app = new Vue({
           localStorage['email'] = this.email
         }
 
-        const response = await axios.post('http://localhost:3001/securify', {
+        const response = await axios.post(securifyURL, {
           email: this.email
         })
 
@@ -25,13 +29,16 @@ var app = new Vue({
 
         this.socket.on(response.data.requestId, (response) => {
           console.log('received a response !', response)
-          const { validated, reason } = response
+          const { validated, reason, token } = response
           this.connected = validated
           
-          localStorage['email'] = this.email
+          localStorage.setItem('email', this.email)
           
           if (!validated) {
             this.error = `Login denied: ${reason}`
+          } else {
+            localStorage.setItem('securifyToken', token)
+            this.securifyToken = token
           }
         })
       } catch (e) {
@@ -42,10 +49,35 @@ var app = new Vue({
         }
         console.error(e)
       }
+    },
+    async validateToken () {
+      if (!securifyToken) {
+        return
+      }
+
+      try {
+        const client = axios.create({
+          headers: { 'Authorization': this.securifyToken }
+        })
+        // TODO: gestion des erreurs propre
+        const response = await client.get(profileURL)
+        const { user } = response.data
+        if (user._id) {
+          this.connected = true
+        }
+      } catch (e) {
+        console.log('token verification failed:', e.response && e.response.data)
+        // Possible reasons:
+        //  - invalid token
+        //  - expired token
+        //  - server down
+      }
     }
   },
   created () {
-    this.socket = io('http://localhost:3001')
-    this.email = localStorage['email']
+    this.socket = io(socketURL)
+    this.email = localStorage.getItem('email')
+    this.securifyToken = localStorage.getItem('securifyToken')
+    this.validateToken()
   }
 })
